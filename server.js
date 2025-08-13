0921a4576ec919c1379662b885407cd3074ff78d
FatimaZelay <fatima.elay@gmail.com>
2025-08-13 16:11:22 +0200

    feat: #8

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 console.log('Hier komt je server voor Sprint 12.')
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from 'express'
+
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
+
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express()
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static('public'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
