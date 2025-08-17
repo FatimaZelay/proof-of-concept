6338f9246d9dc814cf740022b35ab9bd2d558423
FatimaZelay <fatima.elay@gmail.com>
2025-08-17 14:37:56 +0200

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,29 +1,32 @@
-const filterForm = document.querySelector(".filter-bar form");
-const dropdown = document.getElementById("category-filter");
+// const filterForm = document.querySelector(".filter-bar form");
+// const dropdown = document.getElementById("category-filter");
 
-if (filterForm && dropdown) {
-  dropdown.addEventListener("change", e => {
-    e.preventDefault(); // Stop full page reload
-    filterCards(dropdown.value);
-  });
-}
+// if (filterForm && dropdown) {
+//   dropdown.addEventListener("change", e => {
+//     e.preventDefault(); // Stop full page reload
+//     filterCards(dropdown.value);
+//   });
+// }
 
-function filterCards(category) {
-  const updateCards = () => {
-    document.querySelectorAll(".card").forEach(card => {
-      card.style.display =
-        category === "all" || card.dataset.category === category
-          ? "block"
-          : "none";
-    });
-  };
+// function filterCards(category) {
+//   const updateCards = () => {
+//     document.querySelectorAll(".card").forEach(card => {
+//       card.style.display =
+//         category === "all" || card.dataset.category === category
+//           ? "block"
+//           : "none";
+//     });
+//   };
 
-  if (document.startViewTransition) {
-    document.startViewTransition(updateCards);
-  } else {
-    updateCards();
-  }
-}
+//   if (document.startViewTransition) {
+//     document.startViewTransition(updateCards);
+//   } else {
+//     updateCards();
+//   }
+// }
+
+  const btn = document.querySelector('.search-btn');
+
   btn.addEventListener('click', () => {
     btn.classList.add('spinning');
 
